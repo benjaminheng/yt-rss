ffd3d5d1034b207ea16e701accd38908c36467fe
Benjamin Heng <ben@hbenjamin.com>
2023-09-26 21:41:24 +0800

    Implement fzf and mpv

--- main.go
+++ main.go
@@ -1,11 +1,16 @@
 package main
 
 import (
+	"bytes"
 	"encoding/xml"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
+	"os/exec"
+	"strings"
 	"time"
 
 	"github.com/pkg/errors"
@@ -57,17 +62,68 @@ func getFeed(feedURL string) (*Feed, error) {
 	return feed, nil
 }
 
-func printFeedEntries(entries []FeedEntry) error {
-	for _, v := range entries {
+// buildFZFContent builds the content to show in a fzf instance. This function
+// also returns a map, mapping each line in the fzf content to the
+// corresponding feed entry. The map allows the feed entry to be looked up
+// based on the selected line.
+func buildFZFContent(entries []FeedEntry) (fzfContent string, feedEntryLookup map[string]FeedEntry, err error) {
+	feedEntryLookup = make(map[string]FeedEntry)
+	for i, v := range entries {
 		parsedDate, err := time.Parse(time.RFC3339, v.Published)
 		if err != nil {
-			return err
+			return "", nil, err
 		}
 		formattedDate := parsedDate.Format("02 Jan")
 		line := fmt.Sprintf("[%s] [%s] %s", formattedDate, v.Author.Name, v.MediaGroup.Title)
-		fmt.Println(line)
+
+		feedEntryLookup[line] = v
+
+		fzfContent += line
+		if i < len(entries)-1 {
+			fzfContent += "\n"
+		}
+	}
+	return fzfContent, feedEntryLookup, nil
+}
+
+func selectAndPlay(entries []FeedEntry) error {
+	// Get fzf content
+	fzfContent, feedEntryLookup, err := buildFZFContent(entries)
+	if err != nil {
+		return err
+	}
+
+	// Select in fzf
+	r := strings.NewReader(fzfContent)
+	b := &bytes.Buffer{}
+	err = runShellCommand("fzf", nil, r, b)
+	if err != nil {
+		return err
+	}
+
+	// Parse selection, play in mpv
+	selection := strings.Trim(b.String(), "\n")
+	if selection != "" {
+		if feedEntry, ok := feedEntryLookup[selection]; ok {
+			url := feedEntry.MediaGroup.Content.URL
+			fmt.Fprintf(os.Stderr, "Playing %s\n", url)
+			err := runShellCommand("mpv", []string{url}, nil, os.Stdout)
+			if err != nil {
+				return err
+			}
+		} else {
+			return errors.New("url not found for selection")
+		}
 	}
 	return nil
+}
+
+func runShellCommand(command string, args []string, r io.Reader, w io.Writer) error {
+	cmd := exec.Command(command, args...)
+	cmd.Stderr = os.Stderr
+	cmd.Stdout = w
+	cmd.Stdin = r
+	return cmd.Run()
 }
 
 func main() {
@@ -75,7 +131,8 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	// b, _ := json.Marshal(feed)
-	// fmt.Println(string(b))
-	printFeedEntries(feed.Entries)
+	err = selectAndPlay(feed.Entries)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
